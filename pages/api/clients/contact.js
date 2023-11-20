221ec548d8487886b190e88af817d1ce643b776a
Rodrigo Castro <rodrigocastro101998@gmail.com>
2023-11-19 23:54:50 -0300

    modulo de clientes completo

--- pages/api/clients/contact.js
+++ pages/api/clients/contact.js
@@ -3,12 +3,14 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Disable SSL/TLS verification 
 export default async function handler(req, res) {
 
     const { data } = JSON.parse(req.body)
+    const { id } = JSON.parse(req.body);
 
+    console.log(id);
     console.log(data)
 
     try {
-        const response = await fetch(`https://localhost:7233/v1/contactos/create`, {
-            method: 'POST',
+        const response = await fetch(`https://localhost:7233/v1/cliente/update/${id}`, {
+            method: 'PUT',
             body: JSON.stringify(data),
             headers: {
                 'Content-Type': 'application/json',
@@ -16,12 +18,17 @@ export default async function handler(req, res) {
         });
 
         const contentType = response.headers.get('Content-Type');
+        if (response.status === 204) {
+            // Successful update with no content to return
+        }
         if (contentType && contentType.includes('application/json')) {
             const datas = await response.json();
             res.status(200).json(datas);
         } else {
             console.log('Non-JSON Response:', await response.text());
-            res.status(500).json({ error: 'Internal Server Error' });
+            console.log('Full Response:', response);
+
+            res.status(204).end();
         }
     } catch (error) {
         console.error('Error fetching data:', error.message);
